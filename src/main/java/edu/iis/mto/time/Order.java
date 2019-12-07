40a2e81b75251ee44c8bba2513feeada8092397e
Paweł Siciński <233008@edu.p.lodz.pl>
2019-12-07 12:53:23 +0100

    Applying formatter.

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -2,67 +2,74 @@ package edu.iis.mto.time;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.jetbrains.annotations.NotNull;
 import org.joda.time.DateTime;
 import org.joda.time.Hours;
 
 public class Order {
-	private static final int VALID_PERIOD_HOURS = 24;
-	private State orderState;
-	private List<OrderItem> items = new ArrayList<OrderItem>();
-	private DateTime subbmitionDate;
 
-	public Order() {
-		orderState = State.CREATED;
-	}
+    private static final int VALID_PERIOD_HOURS = 24;
+    private State orderState;
+    private List<OrderItem> items = new ArrayList<>();
+    private DateTime submissionDate;
 
-	public void addItem(OrderItem item) {
-		requireState(State.CREATED, State.SUBMITTED);
+    public Order() {
+        orderState = State.CREATED;
+    }
 
-		items.add(item);
-		orderState = State.CREATED;
+    public void addItem(OrderItem item) {
+        requireState(State.CREATED, State.SUBMITTED);
+        items.add(item);
+        orderState = State.CREATED;
+    }
 
-	}
+    public void submit() {
+        requireState(State.CREATED);
 
-	public void submit() {
-		requireState(State.CREATED);
+        orderState = State.SUBMITTED;
+        submissionDate = new DateTime();
 
-		orderState = State.SUBMITTED;
-		subbmitionDate = new DateTime();
+    }
 
-	}
+    public void confirm() {
+        requireState(State.SUBMITTED);
+        int hoursElapsedAfterSubmission = Hours.hoursBetween(submissionDate, new DateTime())
+                                               .getHours();
 
-	public void confirm() {
-		requireState(State.SUBMITTED);
-		int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, new DateTime()).getHours();
-		if(hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS){
-			orderState = State.CANCELLED;
-			throw new OrderExpiredException();
-		}
-	}
+        if (hoursElapsedAfterSubmission > VALID_PERIOD_HOURS) {
+            orderState = State.CANCELLED;
+            throw new OrderExpiredException();
+        }
+    }
 
-	public void realize() {
-		requireState(State.CONFIRMED);
-		orderState = State.REALIZED;
-	}
+    public void realize() {
+        requireState(State.CONFIRMED);
+        orderState = State.REALIZED;
+    }
 
-	State getOrderState() {
-		return orderState;
-	}
-	
-	private void requireState(State... allowedStates) {
-		for (State allowedState : allowedStates) {
-			if (orderState == allowedState)
-				return;
-		}
+    State getOrderState() {
+        return orderState;
+    }
 
-		throw new OrderStateException("order should be in state "
-				+ allowedStates + " to perform required  operation, but is in "
-				+ orderState);
+    private void requireState(@NotNull State... allowedStates) {
+        for (State allowedState : allowedStates) {
+            if (orderState == allowedState) {
+                return;
+            }
+        }
 
-	}
+        throw new OrderStateException("order should be in state "
+                                      + allowedStates
+                                      + " to perform required  operation, but is in "
+                                      + orderState);
 
-	public static enum State {
-		CREATED, SUBMITTED, CONFIRMED, REALIZED, CANCELLED
-	}
+    }
+
+    public static enum State {
+        CREATED,
+        SUBMITTED,
+        CONFIRMED,
+        REALIZED,
+        CANCELLED
+    }
 }
