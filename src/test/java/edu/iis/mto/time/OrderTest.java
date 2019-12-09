f021708289661ef71d3f34d032b0351e612676a7
Krystian Bąk <krystianb.bk@gmail.com>
2019-12-09 18:28:04 +0100

    added test throwing OrderExpiredException

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.time;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderTest {
+
+    private static final int VALID_PERIOD_HOURS = 24;
+    private Order order;
+
+
+    @Before
+    public void initialize(){
+        order = new Order();
+    }
+
+
+    @Test(expected = OrderExpiredException.class)
+    public void shouldThrowOrderExpiredException() {
+        OrderItem item = new OrderItem();
+        order.addItem(item);
+        order.submit(new DateTime().minus(Period.hours(VALID_PERIOD_HOURS + 1)));
+        order.confirm();
+    }
+}
