3c31dd73bf656b85c50077429c96be2ece9969ac
JustynaKloc <jkloc96@gmail.com>
2019-12-09 22:17:33 +0100

    Add test

--- src/main/java/edu/iis/mto/time/OrderTest.java
+++ src/main/java/edu/iis/mto/time/OrderTest.java
@@ -1,30 +1,38 @@
 package edu.iis.mto.time;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.joda.Period;
-import org.joda.DateTime;
+import org.joda.time.Period;
+import org.joda.time.DateTime;
 
-import java.time.Period;
-
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class OrderTest {
 
-    private static final int VALID_PERIOD_HOURS  = 24;
-    private Order order;
-
-    @Before
-    public void setTests(){
-        order = new Order();
-    }
+    private static final int VALID_PERIOD_HOURS = 24 ;
 
     @Test(expected = OrderExpiredException.class)
     public void shouldThrowException(){
+        Order order = new Order();
         OrderItem orderItem = new OrderItem();
         order.addItem(orderItem);
         order.submit(new DateTime().minus(Period.hours(VALID_PERIOD_HOURS  +1)));
         order.confirm();
     }
+
+    @Test
+    public void shouldAcceptUsingSystemClock(){
+        Order order = new Order();
+        order.addItem(new OrderItem());
+        DateTime submissionDate = new DateTime();
+        submissionDate = submissionDate.minus(Period.millis(1));
+
+        order.submit(submissionDate);
+        order.confirm();
+        order.realize();
+
+        assertThat(order.getOrderState(), is(Order.State.REALIZED));
+    }
+
 
 }
