0274508d3458bfabf411a2efcb82a4c48d197999
dawid.krawczyk <dawid.krawczyk@kernelpanic.com>
2019-12-11 22:09:28 +0100

    add test within time range

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -1,78 +1,80 @@
 package edu.iis.mto.time;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.joda.time.DateTime;
 import org.joda.time.Hours;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
-	private static final int VALID_PERIOD_HOURS = 24;
-	private State orderState;
-	private List<OrderItem> items = new ArrayList<OrderItem>();
-	private DateTime subbmitionDate;
-	private DateTime testDataTime;
+    private static final int VALID_PERIOD_HOURS = 24;
+    private State orderState;
+    private List<OrderItem> items = new ArrayList<OrderItem>();
+    private DateTime subbmitionDate;
+    private DateTime testDataTime;
 
-	public Order() {
-		orderState = State.CREATED;
-	}
-	public void setDataTimeForTest(DateTime dateTime)
-	{
-		this.testDataTime=dateTime;
-	}
-	public void addItem(OrderItem item) {
-		requireState(State.CREATED, State.SUBMITTED);
+    public Order() {
+        orderState = State.CREATED;
+    }
 
-		items.add(item);
-		orderState = State.CREATED;
+    public void setDataTimeForTest(DateTime dateTime) {
+        this.testDataTime = dateTime;
+    }
 
-	}
+    public void addItem(OrderItem item) {
+        requireState(State.CREATED, State.SUBMITTED);
 
-	public void submit() {
-		requireState(State.CREATED);
+        items.add(item);
+        orderState = State.CREATED;
 
-		orderState = State.SUBMITTED;
-		if(testDataTime==null)
-		{
-			subbmitionDate = new DateTime();
-		}
-		else {
-			subbmitionDate=testDataTime;
-		}
+    }
 
-	}
+    public void submit() {
+        requireState(State.CREATED);
 
-	public void confirm() {
-		requireState(State.SUBMITTED);
-		int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, new DateTime()).getHours();
-		if(hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS){
-			orderState = State.CANCELLED;
-			throw new OrderExpiredException();
-		}
-	}
+        orderState = State.SUBMITTED;
+        if (testDataTime == null) {
+            subbmitionDate = new DateTime();
+        } else {
+            subbmitionDate = testDataTime;
+        }
 
-	public void realize() {
-		requireState(State.CONFIRMED);
-		orderState = State.REALIZED;
-	}
+    }
 
-	State getOrderState() {
-		return orderState;
-	}
-	
-	private void requireState(State... allowedStates) {
-		for (State allowedState : allowedStates) {
-			if (orderState == allowedState)
-				return;
-		}
+    public void confirm() {
+        requireState(State.SUBMITTED);
+        int hoursElapsedAfterSubmittion = Hours.hoursBetween(subbmitionDate, new DateTime()).getHours();
+        if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
+            orderState = State.CANCELLED;
+            throw new OrderExpiredException();
+        } else {
+            orderState = State.CONFIRMED;
+        }
 
-		throw new OrderStateException("order should be in state "
-				+ allowedStates + " to perform required  operation, but is in "
-				+ orderState);
+    }
 
-	}
+    public void realize() {
+        requireState(State.CONFIRMED);
+        orderState = State.REALIZED;
+    }
 
-	public static enum State {
-		CREATED, SUBMITTED, CONFIRMED, REALIZED, CANCELLED
-	}
+    public State getOrderState() {
+        return orderState;
+    }
+
+    private void requireState(State... allowedStates) {
+        for (State allowedState : allowedStates) {
+            if (orderState == allowedState)
+                return;
+        }
+
+        throw new OrderStateException("order should be in state "
+                + allowedStates + " to perform required  operation, but is in "
+                + orderState);
+
+    }
+
+    public static enum State {
+        CREATED, SUBMITTED, CONFIRMED, REALIZED, CANCELLED
+    }
 }
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,29 +1,38 @@
-
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
 import edu.iis.mto.time.OrderItem;
 import org.joda.time.DateTime;
 import org.joda.time.Period;
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class OrderTest {
 
     private static final int VALID_PERIOD_HOURS = 24;
     private Order order;
+    private OrderItem orderItem;
 
-    @Before
-    public void setUpTests() {
+    public OrderTest() {
         order = new Order();
+        orderItem = new OrderItem();
+        order.addItem(orderItem);
     }
 
     @Test(expected = OrderExpiredException.class)
     public void shouldThrowOrderExpiredException() {
-        OrderItem orderItem = new OrderItem();
-        order.addItem(orderItem);
         order.setDataTimeForTest(new DateTime().minus(Period.hours(VALID_PERIOD_HOURS + 1)));
         order.submit();
         order.confirm();
+    }
+
+    @Test
+    public void shouldBeValidOrderForWithinTimeRange() {
+        order.submit();
+        order.confirm();
+        order.realize();
+        assertThat(order.getOrderState(), is(Order.State.REALIZED));
     }
 
 } 
